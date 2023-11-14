5b684f257a4ddad8801703faa69fd3cb6c50e524
Patra Kirsani <patrakirsani.234ca035@nitk.edu.in>
2023-11-15 00:21:15 +0530

    commit 5

--- Practice Qusetions/oddevenarray.c
+++ Practice Qusetions/oddevenarray.c
@@ -7,7 +7,7 @@ int main() {
     printf("Enter the size of the array: ");
     scanf("%d", &n);
 
-    int arr[n];
+    int arr[100];
 
     // Input elements of the array
     printf("Enter the elements of the array:\n");
